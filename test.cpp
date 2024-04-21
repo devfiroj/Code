634f54ee6b133144bdf91acead001a8ea72b1b3e
devfiroj <81456862+devfiroj@users.noreply.github.com>
2024-04-21 18:11:02 +0000

    push

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size;
+    cout<<"hello Kochu"<<endl;
+    cin>>size;
+    for(int i=1;i<=size;i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
